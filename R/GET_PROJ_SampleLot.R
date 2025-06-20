ddefd0c37ad0475f01d017f2de701b5685fbfa81
YuanW00 <2000yuan.wang@gmail.com>
2025-06-20 09:49:17 -0400

    Update GET_PROJ_SampleLot.R

--- R/GET_PROJ_SampleLot.R
+++ R/GET_PROJ_SampleLot.R
@@ -16,56 +16,35 @@
 #' @export
 GET_PROJ_SampleLot <- function (site, project, username, password) {
 
+  lot_info <- paste0("NEXTCEA_SAMPLE_LOT?$expand=PROJECT&$filter=PROJECT/any(a:contains(a/Name,'", project, "'))")
   if (site == "Test") {
-    data("test", package = "NData", envir = environment())
-    proj_pages <- test$page[test$PROJECT_Barcode == project]
     data("t_url", package = "NData", envir = environment())
-    int_url <- paste0(t_url, "NEXTCEA_SAMPLE_LOT?$expand=PROJECT&$skiptoken=")
-    last_page <- max(test$page)
+    lot_url <- paste0(t_url, lot_info)
   } else if (site == "Production") {
-    data("prod", package = "NData", envir = environment())
-    proj_pages <- prod$page[prod$PROJECT_Barcode == project]
     data("p_url", package = "NData", envir = environment())
-    int_url <- paste0(p_url, "NEXTCEA_SAMPLE_LOT?$expand=PROJECT&$skiptoken=")
-    last_page <- max(prod$page)
+    lot_url <- paste0(p_url, lot_info)
   } else {
     print("Wrong Site!")
   }
 
   sample_lot <- NULL
-  url <- paste0(int_url, last_page)
   while (TRUE) {
-    response <- GET(url, authenticate(username, password))
+    response <- GET(lot_url, authenticate(username, password))
     data <- fromJSON(content(response, "text"))
     df <- data$value |>
       unnest(where(is.list), names_sep = "_") |>
-      filter(PROJECT_Barcode == project)
+      filter(Active == TRUE) |>
+      select(-c(EntityTypeName, Id, Sequence, Created, Modified, Active, LikedBy, FollowedBy,
+                Locked, CI_LOT_NUM, PROJECT_EntityTypeName, PROJECT_Id, PROJECT_Sequence,
+                PROJECT_Created, PROJECT_Modified, PROJECT_Active, PROJECT_LikedBy,
+                PROJECT_FollowedBy, PROJECT_Locked))
     sample_lot <- as.data.frame(rbind(sample_lot, df))
     if (!is.null(data[["@odata.nextLink"]]) ) {
-      url <- data[["@odata.nextLink"]]
+      lot_url <- data[["@odata.nextLink"]]
     } else {
       break
     }
   }
-
-  if (!is.null(proj_pages)) {
-    for (i in proj_pages) {
-      url <- paste0(int_url, i)
-      response <- GET(url, authenticate(username, password))
-      data <- fromJSON(content(response, "text"))
-      df <- data$value |>
-        unnest(where(is.list), names_sep = "_") |>
-        filter(PROJECT_Barcode == project)
-      sample_lot <- as.data.frame(rbind(sample_lot, df))
-    }
-  }
-
-  sample_lot <- sample_lot |>
-    filter(Active == TRUE) |>
-    select(-c(EntityTypeName, Id, Sequence, Created, Modified, Active, LikedBy, FollowedBy,
-              Locked, CI_LOT_NUM, PROJECT_EntityTypeName, PROJECT_Id, PROJECT_Sequence,
-              PROJECT_Created, PROJECT_Modified, PROJECT_Active, PROJECT_LikedBy,
-              PROJECT_FollowedBy, PROJECT_Locked))
 
   return(sample_lot)
 }
