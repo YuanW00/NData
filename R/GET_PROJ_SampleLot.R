a341eb805416c0dc89250e235985073a8e820342
YuanW00 <2000yuan.wang@gmail.com>
2025-09-03 10:18:35 -0400

    Update GET_PROJ_SampleLot.R

--- R/GET_PROJ_SampleLot.R
+++ R/GET_PROJ_SampleLot.R
@@ -14,7 +14,7 @@
 #' @examples
 #' GET_PROJ_SampleLot("Test", "NP1", "user", "password");
 #' @export
-GET_PROJ_SampleLot <- function (site, project, username, password) {
+GET_PROJ_SampleLot <- function (site, project = "NP237", username, password) {
 
   lot_info <- paste0("NEXTCEA_SAMPLE_LOT?$expand=PROJECT&$filter=PROJECT/any(a:contains(a/Barcode,'", project, "'))")
   if (site == "Test") {
@@ -31,28 +31,37 @@ GET_PROJ_SampleLot <- function (site, project, username, password) {
   while (TRUE) {
     response <- GET(lot_url, authenticate(username, password))
     data <- fromJSON(content(response, "text"))
-    df <- data$value |>
-      unnest(where(is.list), names_sep = "_") |>
-      filter(Active == TRUE) |>
-      select(-any_of(c("EntityTypeName", "Id", "Sequence", "Created",
-                       "Modified", "Active", "LikedBy", "FollowedBy",
-                       "Locked", "CI_LOT_NUM", "COLUMN_STATUS",
-                       "CATALOG_NUMBER", "COMPANY_NAME",
-                       "SUPPLEMENTAL_DOC_1_ANNOTATIONS",
-                       "SUPPLEMENTAL_DOC_2_ANNOTATIONS", "COMMENTS",
-                       "PROJECT_EntityTypeName", "PROJECT_Id",
-                       "PROJECT_Sequence", "PROJECT_Created",
-                       "PROJECT_Modified", "PROJECT_Active", "PROJECT_LikedBy",
-                       "PROJECT_FollowedBy", "PROJECT_Locked",
-                       "SUPPLEMENTAL_DOC_1@odata.mediaContentType",
-                       "SUPPLEMENTAL_DOC_2@odata.mediaContentType")))
-    sample_lot <- as.data.frame(rbind(sample_lot, df))
-    if (!is.null(data[["@odata.nextLink"]]) ) {
-      lot_url <- data[["@odata.nextLink"]]
+    if (length(data$value) == 0) {
+      sample_lot = NULL
+      if (!is.null(data[["@odata.nextLink"]]) ) {
+        lot_url <- data[["@odata.nextLink"]]
+      } else {
+        break
+      }
     } else {
-      break
+      df <- data$value |>
+        unnest(where(is.list), names_sep = "_") |>
+        filter(Active == TRUE) |>
+        select(-any_of(c("EntityTypeName", "Id", "Sequence", "Created",
+                         "Modified", "Active", "LikedBy", "FollowedBy",
+                         "Locked", "CI_LOT_NUM", "COLUMN_STATUS",
+                         "CATALOG_NUMBER", "COMPANY_NAME",
+                         "SUPPLEMENTAL_DOC_1_ANNOTATIONS",
+                         "SUPPLEMENTAL_DOC_2_ANNOTATIONS", "COMMENTS",
+                         "PROJECT_EntityTypeName", "PROJECT_Id",
+                         "PROJECT_Sequence", "PROJECT_Created",
+                         "PROJECT_Modified", "PROJECT_Active", "PROJECT_LikedBy",
+                         "PROJECT_FollowedBy", "PROJECT_Locked",
+                         "SUPPLEMENTAL_DOC_1@odata.mediaContentType",
+                         "SUPPLEMENTAL_DOC_2@odata.mediaContentType")))
+      sample_lot <- as.data.frame(rbind(sample_lot, df))
+      if (!is.null(data[["@odata.nextLink"]]) ) {
+        lot_url <- data[["@odata.nextLink"]]
+      } else {
+        break
+      }
     }
-  }
 
+  }
   return(sample_lot)
 }
